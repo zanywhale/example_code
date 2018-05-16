104873022dd355712b2bbf9ac20b80593e0c480e
zanywhale <zanywhale@gmail.com>
2018-05-16 11:22:58 +0900

    feat: Add custom_syscall.c

--- nostdlib/custom_syscall.c
+++ nostdlib/custom_syscall.c
@@ -1,114 +1,4 @@
-int _memcmp(const void *s1, const void *s2, unsigned int n)
-{
-        unsigned char u1, u2;
-
-        for ( ; n-- ; s1++, s2++) {
-                u1 = * (unsigned char *) s1;
-                u2 = * (unsigned char *) s2;
-        if ( u1 != u2) {
-                return (u1-u2);
-        }
-    }
-}
-
-int _strcmp(const char *s1, const char *s2)
-{
-	for ( ; *s1 == *s2; s1++, s2++)
-		if (*s1 == '\0')
-	    		return 0;
-	return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
-}
-
-int _strncmp(const char *s1, const char *s2, size_t n)
-{
-	for ( ; n > 0; s1++, s2++, --n)
-		if (*s1 != *s2)
-			return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
-		else if (*s1 == '\0')
-			return 0;
-	return 0;
-}
-
-
-size_t _strlen(char *s)
-{
-        size_t sz;
-
-        for (sz=0;s[sz];sz++);
-        return sz;
-}
-
-int _puts(char *str)
-{
-        _write(1, str, _strlen(str));
-        _fsync(1);
-
-        return 1;
-}
-
-char * itoa(long x, char *t)
-{
-        int i;
-        int j;
-
-        i = 0;
-        do
-        {
-                t[i] = (x % 10) + '0';
-                x /= 10;
-                i++;
-        } while (x!=0);
-
-        t[i] = 0;
-
-        for (j=0; j < i / 2; j++) {
-                t[j] ^= t[i - j - 1];
-                t[i - j - 1] ^= t[j];
-                t[j] ^= t[i - j - 1];
-        }
-
-        return t;
-}
-
-void Memset(void *mem, unsigned char byte, unsigned int len)
-{
-        unsigned char *p = (unsigned char *)mem; 
-        int i = len;
-        while (i--) {
-                *p = byte;
-                p++;
-        }
-}
-
-void _memcpy(void *dst, void *src, unsigned int len)
-{
-        int i;
-        unsigned char *s = (unsigned char *)src;
-        unsigned char *d = (unsigned char *)dst;
-
-        for (i = 0; i < len; i++) {
-                *d = *s;
-                s++, d++;
-        }
-
-}
-
-int _getdents64(unsigned int fd, struct linux_dirent64 *dirp,
-                    unsigned int count)
-{
-        long ret;
-        __asm__ volatile(
-                        "mov %0, %%rdi\n"
-                        "mov %1, %%rsi\n"
-                        "mov %2, %%rdx\n"
-                        "mov $217, %%rax\n"
-                        "syscall" :: "g"(fd), "g"(dirp), "g"(count));
-        asm ("mov %%rax, %0" : "=r"(ret));
-        return (int)ret;
-}
-
-
-int _prctl(long option, unsigned long arg2, unsigned long arg3, unsigned long arg4, unsigned long arg5)
+int sys_prctl(long option, unsigned long arg2, unsigned long arg3, unsigned long arg4, unsigned long arg5)
 {
         long ret;
         
@@ -122,7 +12,7 @@ int _prctl(long option, unsigned long arg2, unsigned long arg3, unsigned long ar
         asm("mov %%rax, %0" : "=r"(ret));
         return (int)ret;
 }
-int _mprotect(void * addr, unsigned long len, int prot)
+int sys_mprotect(void * addr, unsigned long len, int prot)
 {
         unsigned long ret;
         __asm__ volatile(
@@ -136,7 +26,7 @@ int _mprotect(void * addr, unsigned long len, int prot)
         return (int)ret;
 }
 
-long _ptrace(long request, long pid, void *addr, void *data)
+long sys_ptrace(long request, long pid, void *addr, void *data)
 {
         long ret;
 
@@ -152,7 +42,7 @@ long _ptrace(long request, long pid, void *addr, void *data)
         return ret;
 }
 
-void *_mmap(void *addr, unsigned long len, unsigned long prot, unsigned long flags, long fd, unsigned long off)
+void *sys_mmap(void *addr, unsigned long len, unsigned long prot, unsigned long flags, long fd, unsigned long off)
 {
         long mmap_fd = fd;
         unsigned long mmap_off = off;
@@ -172,7 +62,7 @@ void *_mmap(void *addr, unsigned long len, unsigned long prot, unsigned long fla
         return (void *)ret;
 }
 
-int _munmap(void *addr, size_t len)
+int sys_munmap(void *addr, size_t len)
 {
         long ret;
         __asm__ volatile(
@@ -184,14 +74,14 @@ int _munmap(void *addr, size_t len)
         return (int)ret;
 }
 
-void Exit(long status)
+void sys_exit(long status)
 {
         __asm__ volatile("mov %0, %%rdi\n"
                          "mov $60, %%rax\n"
                          "syscall" : : "r"(status));
 }
 
-long _open(const char *path, unsigned long flags, long mode)
+long sys_open(const char *path, unsigned long flags, long mode)
 {
         long ret;
         __asm__ volatile(
@@ -205,7 +95,7 @@ long _open(const char *path, unsigned long flags, long mode)
         return ret;
 }
 
-int _close(unsigned int fd)
+int sys_close(unsigned int fd)
 {
         long ret;
         __asm__ volatile(
@@ -215,7 +105,7 @@ int _close(unsigned int fd)
         return (int)ret;
 }
 
-int _read(long fd, char *buf, unsigned long len)
+int sys_read(long fd, char *buf, unsigned long len)
 {
          long ret;
         __asm__ volatile(
@@ -228,7 +118,7 @@ int _read(long fd, char *buf, unsigned long len)
         return (int)ret;
 }
 
-long _write(long fd, char *buf, unsigned long len)
+long sys_write(long fd, char *buf, unsigned long len)
 {
         long ret;
         __asm__ volatile(
@@ -241,7 +131,7 @@ long _write(long fd, char *buf, unsigned long len)
         return ret;
 }
 
-int _fstat(long fd, void *buf)
+int sys_fstat(long fd, void *buf)
 {
         long ret;
         __asm__ volatile(
@@ -253,7 +143,7 @@ int _fstat(long fd, void *buf)
         return (int)ret;
 }
 
-int _unlink(const char *path)
+int sys_unlink(const char *path)
 {
 	   long ret;
         __asm__ volatile(
@@ -264,7 +154,7 @@ int _unlink(const char *path)
         return (int)ret;
 }
 
-int _rename(const char *old, const char *new)
+int sys_rename(const char *old, const char *new)
 {
         long ret;
         __asm__ volatile(
@@ -276,7 +166,7 @@ int _rename(const char *old, const char *new)
         return (int)ret;
 }
 
-long _lseek(long fd, long offset, unsigned int whence)
+long sys_lseek(long fd, long offset, unsigned int whence)
 {
         long ret;
         __asm__ volatile(
@@ -290,7 +180,7 @@ long _lseek(long fd, long offset, unsigned int whence)
 
 }
 
-int _fsync(int fd)
+int sys_fsync(int fd)
 {
         long ret;
         __asm__ volatile(
