dd8be29da99a1778d6712ea63ac2a6c379e9cdeb
zanywhale <zanywhale@gmail.com>
2018-05-19 00:50:43 +0900

    fix: Indent

--- nostdlib/custom_lib.c
+++ nostdlib/custom_lib.c
@@ -1,84 +1,82 @@
 int lib_memcmp(const void *s1, const void *s2, unsigned int n)
 {
-        unsigned char u1, u2;
-        for ( ; n-- ; s1++, s2++) {
-                u1 = * (unsigned char *) s1;
-                u2 = * (unsigned char *) s2;
+    unsigned char u1, u2;
+    for ( ; n-- ; s1++, s2++) {
+        u1 = * (unsigned char *) s1;
+        u2 = * (unsigned char *) s2;
         if ( u1 != u2) {
-                return (u1-u2);
+            return (u1-u2);
         }
     }
 }
 
 int lib_strcmp(const char *s1, const char *s2)
 {
-	for ( ; *s1 == *s2; s1++, s2++)
-		if (*s1 == '\0')
-	    		return 0;
-	return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
+    for ( ; *s1 == *s2; s1++, s2++)
+        if (*s1 == '\0')
+           return 0;
+    return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
 }
 
 int lib_strncmp(const char *s1, const char *s2, size_t n)
 {
-	for ( ; n > 0; s1++, s2++, --n)
-		if (*s1 != *s2)
-			return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
-		else if (*s1 == '\0')
-			return 0;
-	return 0;
+    for ( ; n > 0; s1++, s2++, --n)
+        if (*s1 != *s2)
+            return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
+        else if (*s1 == '\0')
+            return 0;
+    return 0;
 }
 
 
 size_t lib_strlen(char *s)
 {
-        size_t sz;
-
-        for (sz=0;s[sz];sz++);
-        return sz;
+    size_t sz;
+    for (sz=0;s[sz];sz++);
+    return sz;
 }
 
 char * lib_itoa(long x, char *t)
 {
-        int i;
-        int j;
-        i = 0;
-        do
-        {
-                t[i] = (x % 10) + '0';
-                x /= 10;
-                i++;
-        } while (x!=0);
+    int i;
+    int j;
+    i = 0;
+    do
+    {
+        t[i] = (x % 10) + '0';
+        x /= 10;
+        i++;
+    } while (x!=0);
 
-        t[i] = 0;
+    t[i] = 0;
 
-        for (j=0; j < i / 2; j++) {
-                t[j] ^= t[i - j - 1];
-                t[i - j - 1] ^= t[j];
-                t[j] ^= t[i - j - 1];
-        }
+    for (j=0; j < i / 2; j++) {
+        t[j] ^= t[i - j - 1];
+        t[i - j - 1] ^= t[j];
+        t[j] ^= t[i - j - 1];
+    }
 
-        return t;
+    return t;
 }
 
 void lib_memset(void *mem, unsigned char byte, unsigned int len)
 {
-        unsigned char *p = (unsigned char *)mem; 
-        int i = len;
-        while (i--) {
-                *p = byte;
-                p++;
-        }
+    unsigned char *p = (unsigned char *)mem; 
+    int i = len;
+    while (i--) {
+        *p = byte;
+        p++;
+    }
 }
 
 void lib_memcpy(void *dst, void *src, unsigned int len)
 {
-        int i;
-        unsigned char *s = (unsigned char *)src;
-        unsigned char *d = (unsigned char *)dst;
+    int i;
+    unsigned char *s = (unsigned char *)src;
+    unsigned char *d = (unsigned char *)dst;
 
-        for (i = 0; i < len; i++) {
-                *d = *s;
-                s++, d++;
-        }
-
+    for (i = 0; i < len; i++) {
+            *d = *s;
+            s++, d++;
+    }
 }
